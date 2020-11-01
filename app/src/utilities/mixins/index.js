f44fa33883e134b52628e4513366df909133df30
Nicolas Constantinou <nicolasconstantinou@Mac-mini.local>
2020-11-01 17:57:41 +0200

    implimented mixins

---
+++ app/src/utilities/mixins/index.js
@@ -0,0 +1,24 @@
+const nameSplitter = (name)=> {
+  const nameSplitted = name.split(' ')
+  const firstName = nameSplitted[0].split('')
+  const lasttName = nameSplitted[1].split('')
+  return firstName[0].toUpperCase() + lasttName[0].toUpperCase()
+}
+
+const backgrandColorSelector= (index)=> {
+  const colors=  [
+    "#2CA5C2",
+    "#E36491",
+    "#4DC5E3",
+    "#E3D436",
+    "#968D2C"
+  ]
+  const color = index%5
+  return {'background': `${colors[color]}`}
+}
+
+
+export {
+  nameSplitter,
+  backgrandColorSelector
+}
