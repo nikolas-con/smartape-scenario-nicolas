0ce027759e742eae29b969514dc26226a88c3e66
Nicolas Constantinou <nicolasconstantinou@Mac-mini.local>
2020-11-01 18:07:38 +0200

    splitted the styling in users screens

--- app/src/components/screens/Users/Card/index.js
+++ app/src/components/screens/Users/Card/index.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { useHistory } from 'react-router-dom'
 import Avatar from '../../../share/Avatar'
+import './style.scss'
 
 const Card = ({ user, index }) => {
   const history = useHistory()
   return (
     <div className="users-card" onClick={() => history.push(`/profile/${user.id}`)} key={user.id}>
       <Avatar name={user.name} index={index}/>
-      <div className="user-card-Details">
+      <div className="users-card-details">
         <span>{user.name}</span>
       </div>
     </div>
--- app/src/components/screens/Users/index.js
+++ app/src/components/screens/Users/index.js
@@ -5,19 +5,20 @@ import getUsers from '../../../utilities/apollo/queries/getUsers'
 import Card from './Card'
 import Spinner from '../../share/Spinner'
 import Layout from '../../share/Layout'
+
 const Users = () => {
 
-  const {loading, data} = useQuery(getUsers)
+  const { loading, data } = useQuery(getUsers)
 
-  if(loading) return <Spinner/>
-  console.log('user')  
+  if (loading) return <Spinner />
+  console.log('user')
   return (
     <Layout>
       <div className="users-container">
-        {data.users.data.map((user, index) => <Card key={user.id} user={user} index={index}/>)}
+        {data.users.data.map((user, index) => <Card key={user.id} user={user} index={index} />)}
       </div>
     </Layout>
-   );
+  );
 }
- 
+
 export default Users;
