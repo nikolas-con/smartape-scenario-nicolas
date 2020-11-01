8b469df9bd08449d03124778e463407b952fc823
Nicolas Constantinou <nicolasconstantinou@Mac-mini.local>
2020-11-01 23:16:12 +0200

    implimented userProfile details

--- app/src/components/screens/UserProfile/Details/index.js
+++ app/src/components/screens/UserProfile/Details/index.js
@@ -1,26 +1,66 @@
 import React from 'react';
 import Avatar from '../../../share/Avatar'
 import './style.scss'
-const Details= ({user})=> {
+const Details = ({ user, index }) => {
   return (
     <div className="details-contener">
-      <div>
-        <span>{user.name}</span>
+      <div className="details-name">
+        <Avatar name={user.name} index={index} />
+        <div style={{ marginTop: '30px' }}>
+          <span className="details-title">{user.name}</span>
+        </div>
       </div>
-      <div>
-        <span>{user.username}</span>
-        <span>{user.id}</span>
-        <span>{user.website}</span>
-        <span>{user.phone}</span>
-        <span>{user.email}</span>
+      <div className="details-user-informetions">
+        <div>
+          <p>
+            <span className="details-title">Username</span>
+            <span className="details-value">{user.username}</span>
+          </p>
+          <p>
+            <span className="details-title">ID</span>
+            <span className="details-value">{user.id}</span>
+          </p>
+          <p>
+            <span className="details-title">Website</span>
+            <span className="details-value">{user.website}</span>
+          </p>
+        </div>
+        <div>
+          <p>
+            <span className="details-title">Phone</span>
+            <span className="details-value">{user.phone}</span>
+          </p>
+          <p>
+            <span className="details-title">Email</span>
+            <span className="details-value">{user.email}</span>
+          </p>      
+        </div>
       </div>
-      <div>
-        <span>{user.address.city}</span>
-        <span>{user.address.street}</span>
-        <span>{user.address.suite}</span>
-        <span>{user.address.zipcode}</span>
+      <div className="details-user-address">
+        <div>
+          <p>
+            <span className="details-title">Address</span>
+          </p>
+        </div>
+        <div>
+          <p>
+            <span className="details-title">Street</span>
+            <span className="details-value">{user.address.street}</span>
+          </p>
+          <p>
+            <span className="details-title">Suite</span>
+            <span className="details-value">{user.address.suite}</span>
+          </p>
+          <p>
+            <span className="details-title">City</span>
+            <span className="details-value">{user.address.city}</span>
+          </p>
+          <p>
+            <span className="details-title">Zip Code</span>
+            <span className="details-value">{user.address.zipcode}</span>
+          </p>
+        </div>
       </div>
-      
     </div>
   )
 }
