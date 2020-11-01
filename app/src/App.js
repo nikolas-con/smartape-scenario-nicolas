91633346419b0e2023f244ca3aeeb102a6bf6184
Nicolas Constantinou <nicolasconstantinou@Mac-mini.local>
2020-11-01 17:56:28 +0200

    romove context

--- app/src/App.js
+++ app/src/App.js
@@ -1,26 +1,16 @@
 import React, { useState } from 'react'
-import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
-import NavicationBar from './components/navicationBar'
-import UserProfile from './screens/UserProfile'
-import Users from './screens/Users'
-import Context from './utilities/context'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import UserProfile from './components/screens/UserProfile'
+import Users from './components/screens/Users'
 
 const App = () => {
 
-  const [userId, setUserId] =useState('')
-  const value = {
-    userId,
-    setUserId
-  }
   return (
     <Router>
-      <Context.Provider value={value}>
-        <NavicationBar/>
-        <Switch>
-          <Route path={["/", "/users"]}  component={Users} exact/>
-          <Route path="/profile/:id"  component={UserProfile} exact/>
-        </Switch>
-      </Context.Provider>
+      <Switch>
+        <Route path={["/", "/users"]} component={Users} exact />
+        <Route path="/profile/:id" component={UserProfile} exact />
+      </Switch>
     </Router>
   );
 }
