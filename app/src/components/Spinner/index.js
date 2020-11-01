71b72694aa31ed9a85d8428e1afdfad5d8ef0c07
Nicolas Constantinou <nicolasconstantinou@Mac-mini.local>
2020-11-01 13:34:33 +0200

    build spinner

---
+++ app/src/components/Spinner/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSpinner } from '@fortawesome/free-solid-svg-icons'
+import './style.scss'
+const Spinner =()=> {
+  return(
+    <div className="spiner-container">
+      <FontAwesomeIcon size="9x" color="#4a4a4b" icon={faSpinner} spin/>
+    </div>
+  )
+}
+
+export default Spinner
