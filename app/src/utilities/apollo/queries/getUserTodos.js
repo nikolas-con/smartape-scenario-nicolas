7d92dadecb16930aa1272420a054f387534455d0
Nicolas Constantinou <nicolasconstantinou@Mac-mini.local>
2020-11-02 10:51:52 +0200

    fixed getUserTodos query to get only uncompleted todos

--- app/src/utilities/apollo/queries/getUserTodos.js
+++ app/src/utilities/apollo/queries/getUserTodos.js
@@ -1,29 +1,29 @@
 import qql from 'graphql-tag'
 
 const getUserTodos = qql`
-  query getUserTodos($id: ID!) {
-    user (id: $id) {
-      username,
-      name,
-      email,
-      website,
-      phone,
+query getUserTodos($id: ID!, $options: PageQueryOptions!) {
+  user (id: $id ) {
+    username,
+    name,
+    email,
+    website,
+    phone,
+    id,
+    address {
+      street,
+      suite,
+      city,
+      zipcode
+    }
+    todos (options: $options) {
+      data {
       id,
-      address {
-        street,
-        suite,
-        city,
-        zipcode
-      }
-      todos {
-        data {
-        id,
         title,
         completed
-        }
       }
     }
   }
+}
 `
 
 export default getUserTodos
